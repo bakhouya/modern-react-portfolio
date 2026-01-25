8e49bd31558722c4d31ad103b75db6098e0ce9ba
bamoos <kamfour1997@gmail.com>
2026-01-25 14:29:11 +0100

    Complete structure use basic style, routers

---
+++ src/views/layouts/footer.jsx
@@ -0,0 +1,15 @@
+
+
+
+
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer flex_center_center">
+         <p>© 2026 Mon Application. Tous droits réservés.</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/views/pages/index.jsx
@@ -0,0 +1,14 @@
+
+
+
+
+const Index = () => {
+    return (
+        <div>
+            <h1 className="text text_base">Page d'Accueil</h1>
+            <p>Bienvenue sur notre site !</p>
+        </div>
+    );
+};
+
+export default Index ;
