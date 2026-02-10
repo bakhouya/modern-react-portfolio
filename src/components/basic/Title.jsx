80c3abd578b7b133a1c15dd83832f12ff45f2626
bamoos <kamfour1997@gmail.com>
2026-02-10 18:57:58 +0100

    Chnge style title dynamic

--- src/components/basic/Title.jsx
+++ src/components/basic/Title.jsx
@@ -5,9 +5,9 @@
 export default function Title({title, description}) {
     return (
         <div className="title_section">
-            <div className="text text-3xl text-center text_primary text-semibold title" data-aos="zoom-in">
+            <h3 className="text text-3xl text-center text_primary title" data-aos="zoom-in">
                 {title}
-            </div>
+            </h3>
             <div className="text text-base text-center my_start_1" data-aos="fade-up">
                 {description}
             </div>
