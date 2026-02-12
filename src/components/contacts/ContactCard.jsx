8c528085f431c15b646aa1b0e644fd8aae6edc2b
bamoos <kamfour1997@gmail.com>
2026-02-12 21:16:54 +0100

    fixed: font creen handler

--- src/components/contacts/ContactCard.jsx
+++ src/components/contacts/ContactCard.jsx
@@ -5,15 +5,14 @@ export default function CardContact({title, description, type, phone, children})
 
     return (
         <a href={type ? `${type}:${description}` : description}  
-
             className="item_contact" 
             rel="noopener noreferrer" target="_blank" data-aos="fade-up" data-aos-delay={150}>  
             <div className="flex_start_center">                             
                 <div className="flex_start_center">{children}</div>
                 <div className="slach"></div>
-                <div className="">
+                <div className="info_flex">
                     <div className="text text-lg text-medium text_base_dark">{title}</div>
-                    <div className="text text-base">
+                    <div className="text text-base text_psis">
                         {type ? description : phone}
                     </div>
                 </div>
